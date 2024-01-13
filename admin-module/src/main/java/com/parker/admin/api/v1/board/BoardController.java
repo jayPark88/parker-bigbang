d13ef5b593986a0b5626ea176e085d497f7e5537
jayPark88 <jaypark8282@gmail.com>
2024-01-14 00:36:20 +0900

    add: 공통으로 사용될 baseInfoEntity 추가, Board JPA 단일 테이블 매핑 전략으로 엔터티 추가

---
+++ admin-module/src/main/java/com/parker/admin/api/v1/board/BoardController.java
@@ -0,0 +1,12 @@
+package com.parker.admin.api.v1.board;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/v1/board")
+@RestController
+public class BoardController {
+
+}
